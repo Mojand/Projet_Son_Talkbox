0a94d273e3f765289421ae52756de06406c557ad
celinelestrohan <86303159+celinelestrohan@users.noreply.github.com>
2021-11-15 17:22:56 +0100

    Add files via upload

--- traitement_audio.py
+++ traitement_audio.py
@@ -1,9 +1,11 @@
-#Nicolas Obin
 
 import matplotlib.pyplot as plt
 import librosa.display
 import os
+import numpy as np
 from scipy import signal
+import IPython.display as ipd
+import soundfile as sf
 
 def load_vocal_audio(audio_path):
     """Load a vocal audio.
@@ -25,9 +27,17 @@ def load_vocal_audio(audio_path):
 
     plt.figure(figsize=(14, 5))
     librosa.display.waveplot(audio , sr=sr)
-    plt.show()
+    #plt.show()
 
     return audio, sr 
+
+def read_vocal_audio(audio,sr):
+    """Read a vocal audio."""
+    ipd.Audio(audio, rate=sr)
+
+def save_vocal_audio(audio,sr):
+    """Save a vocal audio."""
+    sf.write("audio/pyaudio_output.wav", audio, sr)
 
 def segm_vocal_audio(audio,sr):
     """Ségmenter un signal audio en segments de 20ms.
@@ -38,34 +48,21 @@ def segm_vocal_audio(audio,sr):
 
     Returns:
         frames (np.ndarray) : les segments de 20ms du signal audio
+        nb_ech_segm : nombre d'echantillons par segment de 20ms
 
     """
 
-    segm_length=int(0.02*sr) #Un segment de 20ms correspond à 441 points.
-    print("taille_frames="+str(segm_length)) 
+    nb_ech_segm=int(0.02*sr) #Un segment de 20ms correspond à 441 points.
+    print("taille_frames="+str(nb_ech_segm)) 
 
-    frames=librosa.util.frame(audio,frame_length=segm_length,hop_length=segm_length,axis=0) #Le signal est divisé en 168 paquets
+    frames=librosa.util.frame(audio,frame_length=nb_ech_segm,hop_length=nb_ech_segm,axis=0) #Le signal est divisé en 168 paquets
     print("nb_frames="+str(frames.shape)) 
 
-    return frames
-
-
-def visualize_vocal_audio_spec(X,sr):
-    """Répresentation du spectrogramme.
-
-    Args:
-        X (np.ndarray): le signal audio en fréquentiel
-        sr (float): The sample rate of the audio file
-
-    """
-    Xdb = librosa.amplitude_to_db(abs(X))
-    plt.figure(figsize=(14, 5))
-    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-    plt.show()
+    return frames,nb_ech_segm
 
 
 
-def apply_window(audio,sr):
+def apply_window(audio,nb_ech_segm):
     """Fenetrage du signal audio.
 
     Args:
@@ -73,26 +70,40 @@ def apply_window(audio,sr):
         sr (float): The sample rate of the audio file
 
     Returns:
-        X (np.ndarray) : la signal fenetré dans le domaine fréquentiel
+        audio_window (np.ndarray) : la signal fenetré dans le domaine fréquentiel
 
     """
 
-    window=signal.windows.hamming(441)
+    window=signal.windows.hamming(nb_ech_segm)
     plt.plot(window)
 
-    X_avant = librosa.stft(audio,n_fft=441)
-    visualize_vocal_audio_spec(X_avant,sr)
+    audio_window=[a*w for a,w in zip(audio,window)]
 
-    X = librosa.stft(audio, window=window, n_fft=441)
-    visualize_vocal_audio_spec(X,sr)
-
-    return X
-
-def reverse_temporal(audio_freq):
-    audio=librosa.istft(audio_freq)
+    return audio_window
 
 
+
+def concatenate(segm_audio) :
+    """Reconcaténer les trames de  20 ms en un signal vocal"""
+    return np.concatenate(segm_audio)
+
+
+##################################################################
+
+#Chargement de l'audio
 audio, sr=load_vocal_audio('audio/voix.wav')
-audio_segm=segm_vocal_audio(audio,sr)
-print(audio_segm[50])
+
+#Segmentation en segments de 20ms
+audio_segm,nb_ech_segm=segm_vocal_audio(audio,sr)
+
+#Fenetre de Hamming
+audio_window=[]
+for i in range (len(audio_segm)) :
+    audio_window.append(apply_window(audio_segm[i],nb_ech_segm))
+
+#Concatenation des trames de 20ms
+audio_conc=concatenate(audio_window)
+
+#Enregistrement du résultat obtenu
+save_vocal_audio(audio_conc,sr)
 
