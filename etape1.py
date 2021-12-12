445d0ddb3c316c1854651db856f673c95e6977e9
mtheuniss <theunissen.mathilde@gmail.com>
2021-12-12 18:37:40 +0100

    ajout temps d execution

--- etape1.py
+++ etape1.py
@@ -10,19 +10,21 @@ import IPython.display as ipd
 import soundfile as sf
 import copy
 from LPC import filtre
+import time
 
 from traitement_audio import apply_window
 
 audio_bruit, sr1=load('audio/bruit_blanc.wav')
-audio_voix, sr2=load('audio/A.wav')
 
-# on enlève les zeros
+# on enlève les zeros sur le signal de parole
 y, sr = librosa.load('audio/A.wav', duration=0.020, offset=1, sr=sr2)
 
 y = np.squeeze(apply_window(y,len(y)))
 
-#recherche des paramètres du filtre sur ce signal
+#recherche des paramètres du filtre sur ce signal puis on applique le filtre sur le bruit
+start_time = time.time()
 bruit_filtre = filtre(audio_voix,audio_bruit,10,"Durbin") #"Durbin" ou "Rinverse"
+print("--- %s seconds ---" % (time.time() - start_time))
 
 plt.figure(1)
 plt.plot(bruit_filtre)
